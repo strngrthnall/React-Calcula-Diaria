55f57e3826a1260c71a60f38f53fd0f70a8f8eb8
Marcos Vinicius <85797701+strngrthnall@users.noreply.github.com>
2023-11-29 18:02:20 -0300

    Finalização do Header

--- calculadora-de-diaria/src/components/Header/index.jsx
+++ calculadora-de-diaria/src/components/Header/index.jsx
@@ -1,7 +1,31 @@
+import {
+  header,
+  containerLogo,
+  logo,
+  containerTitle,
+  title,
+  containerRepoLink,
+  repoLink
+} from './Header.module.css'
+
+const imgUrl = 'https://raw.githubusercontent.com/strngrthnall/React-Calcula-Diaria/main/calculadora-de-diaria/src/assets/icon.png'
+const repoImg = 'https://raw.githubusercontent.com/strngrthnall/React-Calcula-Diaria/main/calculadora-de-diaria/src/assets/icons8-github.svg'
+const linkRepo = 'https://github.com/strngrthnall/React-Calcula-Diaria'
+
 const Header = () => {
   return (
-    <header>
-      <img src="../../assets/icon.png" alt="Icone de Calculadora" />
+    <header className={ header }>
+      <div className={ containerLogo }>
+        <img className={ logo } src={imgUrl} alt="Icone de Calculadora" />
+      </div>
+      <div className={containerTitle}>
+        <h1 className={ title }>Calculadora de Diarias</h1>
+      </div>
+      <div className={containerRepoLink}>
+        <a href={ linkRepo }>
+          <img className={ repoLink } src={repoImg} alt="Github Logo" />
+        </a>
+      </div>
     </header>
   )
 }
